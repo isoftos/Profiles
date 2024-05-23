bfe7212ceecea6f8ac3af60dd87dc4d21387d7a0
iSoftOS - 凌霄的聚宝盆 <168404326+isoftos@users.noreply.github.com>
2024-05-23 09:00:35 +0800

    Create session.js

---
+++ QuantumultX/Scripts/session.js
@@ -0,0 +1,21 @@
+/******************************
+
+脚本名称: session 番茄钟
+下载地址：商店下载
+更新时间：2024-05-23
+*******************************
+
+[rewrite_local]
+https://api.stayinsession.com/auth/login url script-response-body https://raw.githubusercontent.com/isoftos/Profiles/QuantumultX/Scripts
+/session.js
+
+[mitm] 
+hostname = api.stayinsession.com
+
+*******************************/
+
+var objc = JSON.parse($response.body);
+objc.subscription_status = 'pro'
+objc.subscription_expiry_date = '2029-05-29T13:42:53Z'
+
+$done({body : JSON.stringify(objc)});
